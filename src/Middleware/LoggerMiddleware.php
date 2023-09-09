d45d855dc5ba1370ea04f81edcf3d409b52d5ae1
Vyacheslav Bazhenov <maffinca69@gmail.com>
2023-09-09 15:03:17 +0400

    Try fix cors

--- src/Middleware/LoggerMiddleware.php
+++ src/Middleware/LoggerMiddleware.php
@@ -24,27 +24,11 @@ class LoggerMiddleware implements MiddlewareInterface
      */
     public function handle(Request $request, \Closure|Closure $next, string $guard = null): mixed
     {
-        if ($request->isMethod(RequestAlias::METHOD_OPTIONS)) {
-            return response()->json(['method' => RequestAlias::METHOD_OPTIONS], 200, $headers);
-        }
-
         /** @var Response $response */
         $response = $next($request);
 
         $loggerDTO = $this->loggerDTOAssembler->create($request, $response);
         $this->loggerService->log($loggerDTO);
-
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
-        ];
-
-        foreach($headers as $key => $value) {
-            $response->header($key, $value);
-        }
 
         return $response;
     }
